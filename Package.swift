b9f7cc390f209da5fc53bd31f2b1e67179a5155b
Oleg Dreyman <dreymonde@me.com>
2020-12-06 16:59:38 +0200

    update Swift PM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,25 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Alba"
+    name: "Alba",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "Alba", type: .static, targets: ["Alba"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Alba",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "AlbaTests",
+            dependencies: ["Alba"]
+        )
+    ]
 )
